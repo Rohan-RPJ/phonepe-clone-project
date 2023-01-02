abe4d408c1f622c0d233300665b2a3bf63b89195
rpjtesting <rpjtesting@gmail.com>
2023-01-02 23:08:18 +0530

    Press Cards Mobile Respponsive done

--- components/MainContent8CardsPress.jsx
+++ components/MainContent8CardsPress.jsx
@@ -4,18 +4,20 @@ import GradientButton from "./GradientButton";
 const MainContent8CardsPress = () => {
   return (
     <div
-      className={`w-full flex flex-col space-y-14 items-center justify-center pl-[145px] pr-[131px] pt-[122px] pb-[94px] bg-[#f6f2ff]`}
+      className={`w-full flex flex-col space-y-14 items-center justify-center px-6 sm:pl-[145px] sm:pr-[131px] pt-[62px] sm:pt-[122px] pb-[94px] bg-[#f6f2ff]`}
     >
       <div className={`flex flex-col gap-2 items-center justify-center pr-3`}>
         <p className={`text-[2.5rem] text-[#361968] font-semibold`}>
           In the Press
         </p>
-        <p className={`text-sm font-[500]`}>
+        <p className={`text-sm font-[400] sm:font-[500]`}>
           Read what the press has to say about us.
         </p>
       </div>
 
-      <div className={`w-full flex gap-12 items-center justify-center`}>
+      <div
+        className={`w-full flex flex-col sm:flex-row gap-12 items-center justify-center`}
+      >
         {[
           {
             date: "10 November",
@@ -45,20 +47,21 @@ const MainContent8CardsPress = () => {
           return (
             <div
               key={index}
-              className={`w-1/3 h-[400px] flex flex-col items-start justify-between text-gray-600 pl-6 pt-9 pr-5 bg-white border border-gray-300 rounded-xl hover:shadow-2xl transition-shadow cursor-pointer group`}
+              className={`w-full sm:w-1/3 h-[322px] sm:h-[400px] flex flex-col items-start justify-between text-gray-600 pl-6 pt-9 pr-5 bg-white border border-gray-300 rounded-xl hover:shadow-2xl transition-shadow cursor-pointer group`}
             >
               <p className={`text-base font-light`}>{card.date}</p>
               <p className={`text-[1.375rem] font-medium pb-8 pr-[19px]`}>
                 {card.title}
               </p>
-              <p className={`text-[15px] leading-6 font-light`}>{card.desc}</p>
-              <div className={`flex w-full items-center justify-between`}>
-                <p className={`text-lg text-black font-normal pt-8`}>
+              <p className={`hidden sm:block text-[15px] leading-6 font-light`}>
+                {card.desc}
+              </p>
+              <div className={`w-full flex items-center justify-between pt-8`}>
+                <p className={`text-lg text-black font-normal`}>
                   {card.author}
                 </p>
                 <ArrowLongRightIcon
-                  width={45}
-                  className={`text-[#6739B7] invisible group-hover:visible`}
+                  className={`text-[#6739B7] sm:invisible sm:group-hover:visible w-[30px] sm:w-[45px]`}
                 />
               </div>
               <div className={`w-full px-4`}>
