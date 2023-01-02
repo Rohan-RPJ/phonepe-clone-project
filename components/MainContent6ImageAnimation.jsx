c9a86baebcae9070ba0a84a3e5a0c40bf0d75285
rpjtesting <rpjtesting@gmail.com>
2023-01-03 00:15:04 +0530

    Updated MainContent6 for mobile view

--- components/MainContent6ImageAnimation.jsx
+++ components/MainContent6ImageAnimation.jsx
@@ -12,7 +12,6 @@ const MainContent6ImageAnimation = ({ isMobileNav }) => {
     const observer = new IntersectionObserver(function (entries, observer) {
       entries.forEach((entry) => {
         if (entry.isIntersecting) {
-          console.log("????");
           setMyRefScrolled(true);
         } else {
           // setMyRefScrolled(false)
@@ -25,28 +24,27 @@ const MainContent6ImageAnimation = ({ isMobileNav }) => {
 
   return (
     <div
-      className={`w-full h-full relative bg-gradient-to-b from-[#ddd8fa] to-[#ddd8fa] pt-[350px] -translate-y-[140px] mb-[350px] z-0`}
+      className={`w-full h-full relative bg-gradient-to-b from-[#ddd8fa] to-[#ddd8fa] pt-[350px] -translate-y-[140px] mb-[410px] sm:mb-[350px] z-0`}
     >
       <div
         className={`w-full h-full absolute bg-gradient-to-b from-[#ddd8fa] to-[#fff]`}
       >
         <div
-          className={`w-full h-full flex flex-col items-center justify-center`}
+          className={`w-full h-full flex flex-col items-center justify-center relative`}
         >
-          <span
-            className={`text-[2rem] sm:text-[2.625rem] text-[#361968] font-bold flex flex-col items-center justify-center`}
+          <div
+            className={`text-[2rem] sm:text-[2.625rem] text-[#361968] font-bold flex flex-col items-center`}
           >
             <h2>At ~3.5 Crore</h2>
-            <h2>stores, apps, websites & more</h2>
-          </span>
+            <h2 className="text-center">stores, apps, websites & more</h2>
+          </div>
 
           <GradientButton text={"Partner with us"} />
 
           <div
-            className={`w-full h-[600px] sm:h-full px-32 py-4 -translate-x-[1000px] sm:-translate-x-[1500px] ${
-              myRefScrolled && "translate-x-0"
+            className={`w-full h-[600px] sm:h-full px-16 sm:px-32 py-8 sm:py-4 -translate-x-[1000px] sm:-translate-x-[1500px] ${
+              myRefScrolled && "translate-x-0 sm:translate-x-0"
             } transition-transform duration-[1000ms]`}
-            ref={myRef}
           >
             <Image
               src={
@@ -57,6 +55,10 @@ const MainContent6ImageAnimation = ({ isMobileNav }) => {
               priority
             />
           </div>
+          <div
+            className={`invisible absolute top-[75%] sm:top-[80%] right-1/2`}
+            ref={myRef}
+          ></div>
         </div>
       </div>
     </div>
